95746be07ed695e82e37e827bb94d286678445ae
jim <jimorcheson@gmail.com>
2022-08-19 07:50:49 -0400

    Create minimalist GUI app

--- src/jimoSDR/src/jimoSDR.cpp
+++ src/jimoSDR/src/jimoSDR.cpp
@@ -1,7 +1,18 @@
-#include <iostream>
+#include <xtd/xtd>
+
+using namespace xtd;
+using namespace xtd::forms;
+
+class jimo_form : public form
+{
+    public:
+        jimo_form() {
+        }
+};
 
 int main()
 {
-    std::cout << "Hello, World!\n";
+    jimo_form form1 {};
+    application::run(form1);
     return EXIT_SUCCESS;
 }
