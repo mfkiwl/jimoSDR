02c1d59aa1282a3810ead373a27bab1f6d634936
jim <jimorcheson@gmail.com>
2022-08-19 13:27:23 -0400

    Add left and right panels to main_form

--- jimoSDR/include/main_form.h
+++ jimoSDR/include/main_form.h
@@ -2,11 +2,30 @@
 
 #include <xtd/xtd>
 
+using namespace xtd::drawing;
+
 namespace jimo_sdr
 {
+    const color left_panel_background = color::from_argb(0xFF000000);
+    const color right_panel_background = color::from_argb(0xFF202020);
+
     class main_form : public xtd::forms::form
     {
         public:
             main_form();
+        protected:
+            void on_form_closing(xtd::forms::form_closing_event_args& e) 
+                override {
+                    e.cancel(xtd::forms::message_box::show(*this, 
+                        "Are you sure you want exit?", 
+                        "Close Form", xtd::forms::message_box_buttons::yes_no, 
+                        xtd::forms::message_box_icon::question) == 
+                            xtd::forms::dialog_result::no);};
+        private:
+            void create_left_panel();
+            void create_right_panel();
+
+            xtd::forms::panel _left_panel;
+            xtd::forms::panel _right_panel;
     };
 }
--- jimoSDR/src/main_form.cpp
+++ jimoSDR/src/main_form.cpp
@@ -1,9 +1,39 @@
 #include "main_form.h"
 
+using namespace xtd;
+using namespace xtd::forms;
+using namespace xtd::drawing;
+
 namespace jimo_sdr
 {
     main_form::main_form()
     {
+        text("jimoSDR");
+        size({1000, 800});
+        start_position(xtd::forms::form_start_position::center_screen);
+        auto_size(false);
 
+        create_left_panel();
+        create_right_panel();
+    }
+
+    void main_form::create_left_panel()
+    {
+        _left_panel.location({0, 0});
+        auto& main_form_client_rect = client_size();
+        _left_panel.size({200, main_form_client_rect.height()});
+        _left_panel.back_color(left_panel_background);
+        *this << _left_panel;
+    }
+
+    void main_form::create_right_panel()
+    {
+        auto& main_form_client_rect = client_size();
+        auto left_panel_size = _left_panel.size();
+        _right_panel.location({left_panel_size.width(), 0});
+        _right_panel.size({main_form_client_rect.width() - left_panel_size.width(),
+            main_form_client_rect.height()});
+        _right_panel.back_color(right_panel_background);
+        *this << _right_panel;
     }
 }
